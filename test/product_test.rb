a8b1e8f9d1c9ca1c89ce723290e4bc76e62b6355
Taylor Keller <takeller17@gmail.com>
2020-03-25 09:18:47 -0600

    Removed extra prodcut initializations

--- test/product_test.rb
+++ test/product_test.rb
@@ -10,34 +10,26 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_exists
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_instance_of Product, @product1
   end
 
   def test_it_has_a_category
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, @product1.category
   end
 
   def test_it_has_a_name
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 'toilet paper', @product1.name
   end
 
   def test_it_has_a_unit_price
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 3.70, @product1.unit_price
   end
 
   def test_it_has_a_quantity
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 10, @product1.quantity
   end
 
   def test_it_can_get_total_price
-    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
-
     assert_equal 37.0, @product1.total_price
     assert_equal 9.0, @product2.total_price
   end
