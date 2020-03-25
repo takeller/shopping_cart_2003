44248f74af0e1f60b5fe8f6480b91e4983e0f313
Taylor Keller <takeller17@gmail.com>
2020-03-25 09:14:09 -0600

    Complete iteration 1

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,23 @@
-class Prodct
+class Product
 
+  attr_reader :category, :name, :unit_price, :quantity
   def initialize(category, name, unit_price, quantity)
     @category = category
-    @name = name_parameter
-    @unit_price = 3.70
-    @quantity = quantity
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
+    @is_hoarded = false
   end
 
   def total_price
     @unit_price * @quantity
   end
+
+  def hoard
+    @is_hoarded = true
+  end
+
+  def is_hoarded?
+    @is_hoarded
+  end 
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -3,41 +3,53 @@ require 'minitest/pride'
 require './lib/product'
 
 class ProductTest < Minitest::Test
+
+  def setup
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  end
+
   def test_it_exists
-    product = Product.new(:paper, 'toilet paper', 3.70, '10')
-    assert_instance_of Product, product
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_instance_of Product, @product1
   end
 
   def test_it_has_a_category
-    skip
-    product = Product.new(:paper, 'toilet paper', 3.70, '10')
-    assert_equal :paper, product.category
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal :paper, @product1.category
   end
 
   def test_it_has_a_name
-    skip
-    product = Product.new(:paper, 'toilet paper', 3.70, '10')
-    assert_equal 'toilet paper', product.name
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal 'toilet paper', @product1.name
   end
 
   def test_it_has_a_unit_price
-    skip
-    product = Product.new(:paper, 'toilet paper', 3.70, '10')
-    assert_equal 3.70, product.unit_price
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal 3.70, @product1.unit_price
   end
 
   def test_it_has_a_quantity
-    skip
-    product = Product.new(:paper, 'toilet paper', 3.70, '10')
-    assert_equal 10, product.quantity
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal 10, @product1.quantity
   end
 
   def test_it_can_get_total_price
-    skip
-    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
-    assert_equal 37.0, product1.total_price
-    assert_equal 9.0, product2.total_price
+    assert_equal 37.0, @product1.total_price
+    assert_equal 9.0, @product2.total_price
+  end
+
+  def test_hoard_and_is_hoarded?
+    assert_equal false, @product1.is_hoarded?
+    assert_equal false, @product2.is_hoarded?
+
+    @product1.hoard
+    @product2.hoard
+
+    assert_equal true, @product1.is_hoarded?
+    assert_equal true, @product2.is_hoarded?
   end
 end
